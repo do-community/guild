f8588fec61c5555887b9f1f3c3494b18f955eacd
= <tnylea@gmail.com>
2020-12-20 09:13:53 -0800

    Adding updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
             @livewire('navigation-dropdown')
 
             <!-- Page Heading -->
-            <header class="bg-white shadow">
+            <header class="bg-gray-900 shadow">
                 <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
