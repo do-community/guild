20934c9b2e3bd2cec1265ad144cba5318ecbb2fc
Bobby Iliev <bobby@bobbyiliev.com>
2020-12-22 17:03:28 +0000

    Livewire shift button component to menu

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -27,8 +27,8 @@
     <div class="relative w-full">
 
         <!-- Shift Start/Stop -->
-        <div class="relative flex w-full p-5">
-            <button class="inline-flex items-center justify-center w-full px-4 py-2 text-xs font-semibold tracking-widest text-center text-white uppercase transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25">Start Shift</button>
+        <div class="relative w-full p-5">
+            @livewire('shifts')
         </div>
 
         @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
--- resources/views/shifts/index.blade.php
+++ resources/views/shifts/index.blade.php
@@ -7,7 +7,44 @@
 
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            @livewire('shifts')
+            <div class="mt-10 sm:mt-0">
+                <div class="md:grid md:grid-cols-3 md:gap-6">
+                    <div class="md:col-span-1">
+                        <div class="px-4 sm:px-0">
+                            <h3 class="text-lg font-medium text-gray-900">Change your shift status!</h3>
+
+                            <p class="mt-1 text-sm text-gray-600">
+                                <p>Current guild {{ auth()->user()->currentTeam->name }}</p>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="mt-5 md:mt-0 md:col-span-2">
+                        @if (session()->has('message'))
+                        <div class="alert alert-success">
+                            {{ session('message') }}
+                        </div>
+                        @endif
+
+                        <div class="overflow-hidden shadow sm:rounded-md">
+                            <div class="px-4 py-5 bg-white sm:p-6">
+                                <div class="grid grid-cols-6 gap-6">
+                                    <div class="col-span-6 sm:col-span-4">
+                                        <h3>Total Hours Worked for current guild: <span>{{  auth()->user()->totalHoursWorked() }}</span></h3>
+                                        <label class="block text-sm font-medium text-gray-700" for="current_password">
+                                            Currently not on shift.
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="flex items-center justify-end px-4 py-3 text-right bg-gray-50 sm:px-6">
+                                @livewire('shifts')
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </x-app-layout>
