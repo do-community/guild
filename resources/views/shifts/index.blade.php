57443d0a357327993db1bb33392037898a3622df
Bobby Iliev <bobby@bobbyiliev.com>
2020-12-23 18:56:11 +0000

    Added shift logs

--- resources/views/shifts/index.blade.php
+++ resources/views/shifts/index.blade.php
@@ -44,7 +44,11 @@
                     </div>
                 </div>
             </div>
-
+            <div class="py-12">
+                <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    @livewire('shift-logs')
+                </div>
+            </div>
         </div>
     </div>
 </x-app-layout>
